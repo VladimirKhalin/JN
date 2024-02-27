ef6a3b6269f9e16423cb6737b4d661aefa644c3b
VK <art.nebar@mail.ru>
2024-02-27 20:05:11 +0300

    Initial Commit

---
+++ Test00001/Test00001App.swift
@@ -0,0 +1,17 @@
+//
+//  Test00001App.swift
+//  Test00001
+//
+//  Created by Vladimir Khalin on 27.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Test00001App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
